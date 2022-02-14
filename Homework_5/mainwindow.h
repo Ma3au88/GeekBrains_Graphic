4142a087dd441be3f96520f246e5128c07c18b7b
Ma3au88 <53112053+Ma3au88@users.noreply.github.com>
2022-02-14 14:16:41 +0500

    Homework_5.1

---
+++ Homework_5/mainwindow.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <QMainWindow>
+#include <QTranslator>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    bool read_only;
+
+private slots:
+    void read_only_switch();
+
+    void darkTheme();
+
+    void lightTheme();
+
+    void openText();
+
+    void createText();
+
+    void saveText();
+
+    void printText();
+
+    void helpAbout();
+
+    void switchLanguage();
+
+private:
+    Ui::MainWindow *ui;
+    QAction* read_only_action;
+    QTranslator translater;
+};
+
