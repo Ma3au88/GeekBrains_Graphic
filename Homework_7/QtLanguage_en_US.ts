2396b9bc5c06ae4592e46f4ddbcbcc0058ec3bfd
Ma3au88 <53112053+Ma3au88@users.noreply.github.com>
2022-02-21 12:23:50 +0500

    Homework_7

---
+++ Homework_7/QtLanguage_en_US.ts
@@ -0,0 +1,71 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>Homework_5</source>
+        <translation type="Homework_5"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="13"/>
+        <location filename="mainwindow.cpp" line="105"/>
+        <source>Только для чтение(ВКЛ)</source>
+        <translation type="Only read (In)"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="18"/>
+        <source>Файл</source>
+        <translation type="File"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="20"/>
+        <location filename="mainwindow.cpp" line="50"/>
+        <source>Открыть</source>
+        <translation type="Open"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="23"/>
+        <location filename="mainwindow.cpp" line="67"/>
+        <source>Сохранить</source>
+        <translation type="Save"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="27"/>
+        <source>О программе</source>
+        <translation type="About"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="31"/>
+        <source>Выход</source>
+        <translation type="Exit"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="34"/>
+        <source>Выбрать тему</source>
+        <translation type="Change theme"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="36"/>
+        <source>Темная тема</source>
+        <translation type="Dark theme"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="39"/>
+        <source>Светлая тема</source>
+        <translation type="Light theme"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="51"/>
+        <location filename="mainwindow.cpp" line="68"/>
+        <source>Текстовый файл(*.txt)</source>
+        <translation type="Text file(*.txt)"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="99"/>
+        <source>Только для чтение(ВЫКЛ)</source>
+        <translation type="Only read (Off)"></translation>
+    </message>
+</context>
+</TS>
